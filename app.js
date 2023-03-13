9d1c3359a88a5ecaf0dc33bba800d70d820f2637
Oliver Paszkiewicz <oliverpaszkiewicz@livewallgroup.com>
2023-03-13 17:35:47 +0100

    Update: formatted the entire code and created comments

--- app.js
+++ app.js
@@ -1,21 +1,79 @@
 'use strict';
 
-// On scroll run the stickyNavbar function
-window.onscroll = function() {stickyNavbar()};
+// All sections from HTML and its top offset
+const aboutSection = document.querySelector('.section-about');
+let topPosAbout = aboutSection.offsetTop;
+const experienceSection = document.querySelector('.section-experience');
+let topPosExperience = experienceSection.offsetTop;
+const contactSection = document.querySelector('.section-contact');
+let topPosContact = contactSection.offsetTop;
+const projectSection = document.querySelector('.section-projects');
+let topPostProjects = projectSection.offsetTop;
+let header = document.getElementById("header");
+let sticky = header.offsetTop;
+const homeSection = document.querySelector('.section-home');
+let topPosHome = homeSection.offsetTop;
 
-var header = document.getElementById("header");
-var sticky = header.offsetTop;
+// Add class list to each elements and removes it
+function animationsOnScroll() {
 
-function stickyNavbar() {
-  // When the top part of the view hits the header
-  // Add sticky class to the header
+  // Page YOffset is bigger or equal to sticky -> add sticky class
   if (window.pageYOffset >= sticky) {
     header.classList.add("sticky")
   } else {
-    // Remove the sticky class from header
+    // Remove the sticky class
     header.classList.remove("sticky");
   }
+
+  // Page YOffset is bigger or equal to sticky -> add appear class
+  if (window.pageYOffset >= topPosAbout - 150) {
+    aboutSection.classList.add('animationAppear');
+  } else if (window.pageYOffset <= topPosAbout - 250) {
+    // Remove the class
+    aboutSection.classList.remove('animationAppear');
+  }
+
+  // Page YOffset is bigger or equal to sticky -> add appear class
+  if (window.pageYOffset >= topPostProjects - 100) {
+    projectSection.classList.add('animationAppear');
+
+  } else if (window.pageYOffset <= topPostProjects - 250) {
+    projectSection.classList.remove('animationAppear');
+    // Remove the class
+  }
+
+  // Page YOffset is bigger or equal to sticky -> add appear class
+  if (window.pageYOffset >= topPosExperience - 100) {
+    experienceSection.classList.add('animationAppear');
+  } else if (window.pageYOffset <= topPosExperience - 250){
+    // Remove the class
+    experienceSection.classList.remove('animationAppear');
+  }
+
+  // Page YOffset is bigger or equal to sticky -> add appear class
+  if (window.pageYOffset >= topPosContact - 100) {
+    contactSection.classList.add('animationAppear');
+  } else if (window.pageYOffset <= topPosContact - 250) {
+    // Remove the class
+
+    contactSection.classList.remove('animationAppear');
+  }
 }
+
+// On scroll fires the animations function
+window.onscroll = function() {animationsOnScroll()};
+
+// Add class list or removes it based on the pageYOffset
+const showOnLoad = function() {
+  if (window.pageYOffset >= topPosHome) {
+    homeSection.classList.add('animationAppear');
+  } else {
+    homeSection.classList.remove('animationAppear');
+  }
+}
+
+// On load fires the showOnLoad animation function
+window.addEventListener('load', showOnLoad());
 
 // Burger menu parent element of three lines
 const burgerMenu = document.querySelector('.header-burger_menu');
