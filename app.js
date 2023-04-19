93d4a4707a7d321532b33b7ceda836129b576783
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2023-04-19 15:16:20 +0200

    Add: animation for the mobile navigation

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 'use strict';
 
-const headerEl = document.getElementById('header');
+const headerEl = document.querySelector('.header');
 const btnEl = document.querySelector('.btn-mobile-nav');
+const navEl = document.getElementById('nav');
 
+// Glassmorphism animation
 btnEl.addEventListener('click', () => {
     headerEl.classList.toggle('nav-open');
+    if (headerEl.classList.contains('nav-open')) {
+        document.body.classList.add('.overflow');
+    } else {
+        document.body.classList.remove('.overflow');
+    }
+    headerEl.classList.toggle('glass');
+    navEl.classList.toggle('glass');
+});
+
+// Sticky navigation
+const sectionHomeMessage = document.querySelector('.section-home');
+const obs = new IntersectionObserver(
+    function (entries) {
+        const ent = entries[0];
+        console.log(ent);
+        if (!ent.isIntersecting) {
+            document.body.classList.add('sticky');
+        } else {
+            document.body.classList.remove('sticky');
+        }
+    }, {
+        root: null,
+        treshold: 0,
+    }
+);
+
+obs.observe(sectionHomeMessage);
+
+// Closing the navigation
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function (link) {
+    link.addEventListener('click', () => {
+        if (link.classList.contains('close-nav')) {
+            headerEl.classList.toggle('nav-open');
+            headerEl.classList.toggle('glass');
+        }
+    });
 });
